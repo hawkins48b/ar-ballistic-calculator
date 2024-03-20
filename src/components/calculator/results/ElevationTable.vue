428eccd1d1580407ccf61a49e3f0f513a9f4983e
hawkins48b <q.mathey@gmail.com>
2024-03-20 01:11:44 +0100

    Add new product icon (#2)
    * add profiles forms
    
    * new feature: add profile in profiles page
    
    * add form validation for profiles
    
    * add cosmetic changes for profiles
    
    * start calculator page
    
    * start add mbpr component
    
    * ui improvements
    
    * add feature ballistic calculator
    
    * quick zero adjustments
    
    * cosmetic changes
    
    * new frontend
    
    * add menu button for desktop
    
    * reskin for profile page
    
    * fix missing reload of profile after deletion
    
    * work on bc calculator
    
    * work on bc calculator
    
    * enhance bc calculator
    
    * improve bc
    
    * improve bc
    
    * fix add profile error
    
    * add github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * add automated version
    
    * 0.1.0
    
    * update package.json with git command
    
    * update
    
    * 0.1.1
    
    * update
    
    * 0.1.2
    
    * update
    
    * 0.1.3
    
    * update
    
    * 0.1.4
    
    * update
    
    * 0.1.5
    
    * update
    
    * 0.1.6
    
    * minor changes
    
    * 0.1.7
    
    * add cw logo icon
    
    * 0.1.8
    
    * 0.1.9
    
    ---------
    
    Co-authored-by: quem <hawkins48b@gmail.com>

---
+++ src/components/calculator/results/ElevationTable.vue
@@ -0,0 +1,216 @@
+<template>
+  <q-table
+    flat
+    :rows="rows"
+    :columns="columns"
+    :visible-columns="visibleColumns"
+    row-key="distance"
+    :loading="loading"
+    :rows-per-page-options="[0]"
+    :class="{'bg-grey-3':!$q.dark.isActive}"
+    hide-pagination
+  >
+    <template #top="props">
+      <div class="col-2 q-table__title">
+        Elevation Table
+      </div>
+
+      <q-space />
+
+      <div
+        v-if="$q.screen.gt.xs"
+        class="col"
+      >
+        <div class="row">
+          <div
+            class="col-4"
+          >
+            <div class="text-bold">
+              Distance
+            </div>
+            <div>
+              <q-toggle
+                v-model="visibleColumns"
+                val="distanceYD"
+                label="Yard"
+              />
+            </div>
+            <div>
+              <q-toggle
+                v-model="visibleColumns"
+                val="distanceM"
+                label="Meter"
+              />
+            </div>
+          </div>
+          <div
+            class="col-4"
+          >
+            <div class="text-bold">
+              Elevation
+            </div>
+            <div>
+              <q-toggle
+                v-model="visibleColumns"
+                val="elevationIN"
+                label="Inch"
+              />
+            </div>
+            <div>
+              <q-toggle
+                v-model="visibleColumns"
+                val="elevationCM"
+                label="Centimeter"
+              />
+            </div>
+          </div>
+          <div
+            class="col-4"
+          >
+            <div class="text-bold">
+              Correction
+            </div>
+            <div>
+              <q-toggle
+                v-model="visibleColumns"
+                val="correctionMOA"
+                label="MOA"
+              />
+            </div>
+            <div>
+              <q-toggle
+                v-model="visibleColumns"
+                val="correctionMRAD"
+                label="MRAD"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <q-select
+        v-else
+        v-model="visibleColumns"
+        multiple
+        borderless
+        dense
+        options-dense
+        :display-value="$q.lang.table.columns"
+        emit-value
+        map-options
+        :options="columns"
+        option-value="name"
+        style="min-width: 150px"
+      />
+
+      <q-btn
+        flat
+        round
+        dense
+        :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+        class="q-ml-md"
+        @click="props.toggleFullscreen"
+      />
+    </template>
+    <template #loading>
+      <q-inner-loading
+        showing
+        color="primary"
+      />
+    </template>
+  </q-table>
+</template>
+
+<script>
+
+import {
+  calculateAdjustments
+} from 'components/calculator/BalisticCalculator.js'
+
+import { LocalStorage } from 'quasar'
+
+export default {
+
+  setup () {
+    return {}
+  },
+  data: function () {
+    return {
+      calcArgs: null,
+      loading: true,
+      rows: [],
+      columns: [
+        {
+          name: 'distanceYD',
+          field: 'distanceYD',
+          label: 'Distance (YD)',
+          format: (val, row) => `${Math.round(val)}`,
+          align: 'left'
+        },
+        // distance
+        {
+          name: 'distanceM',
+          field: 'distanceM',
+          label: 'Distance (M)',
+          format: (val, row) => `${Math.round(val)}`,
+          align: 'left'
+        },
+        {
+          name: 'elevationIN',
+          field: 'elevationIN',
+          label: 'Elevation (IN)',
+          format: (val, row) => `${Math.round(val * 10) / 10}`,
+          align: 'left'
+        },
+        // elevation
+        {
+          name: 'elevationCM',
+          field: 'elevationCM',
+          label: 'Elevation (CM)',
+          format: (val, row) => `${Math.round(val * 10) / 10}`,
+          align: 'left'
+        },
+        // correction
+        {
+          name: 'correctionMOA',
+          field: 'correctionMOA',
+          label: 'Correction (MOA)',
+          format: (val, row) => `${Math.round(val * 10) / 10}`,
+          align: 'left'
+        },
+
+        {
+          name: 'correctionMRAD',
+          field: 'correctionMRAD',
+          label: 'Correction (MRAD)',
+          format: (val, row) => `${Math.round(val * 10) / 10}`,
+          align: 'left'
+        }
+      ],
+      visibleColumns: []
+    }
+  },
+  mounted () {
+    // get ballistic calculator arguments
+    this.calcArgs = JSON.parse(LocalStorage.getItem('bc-args'))
+
+    this.initVisibleColumns()
+
+    this.startBallisticCalculator()
+  },
+  methods: {
+    initVisibleColumns () {
+      console.log('this.calcArgs.resultUnits', this.calcArgs.resultUnits)
+      if (this.calcArgs.resultsUnit === 'YD') {
+        this.visibleColumns = ['distanceYD', 'elevationIN', 'correctionMOA']
+      }
+      if (this.calcArgs.resultsUnit === 'M') {
+        this.visibleColumns = ['distanceM', 'elevationCM', 'correctionMRAD']
+      }
+    },
+    startBallisticCalculator () {
+      this.rows = calculateAdjustments(this.calcArgs)
+      this.loading = false
+    }
+  }
+}
+</script>

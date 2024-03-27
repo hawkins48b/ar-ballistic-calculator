336fec8acd78b43d8736181c2ef549e5ae7d8694
Quentin Mathey <q.mathey@gmail.com>
2024-03-27 01:11:03 +0100

    reinsert listProfilesPage.vue

---
+++ src/pages/profiles/ListProfilesPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="row">
+      <profilesTable
+        :profile-actions="['edit','duplicate','remove']"
+        class="col"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+// imports
+import ProfilesTable from 'components/profiles/list/ProfilesTable.vue'
+
+</script>

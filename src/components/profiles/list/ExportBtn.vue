3a032644ebc189ff627d34eb04e00b96309fc769
Quentin Mathey <q.mathey@gmail.com>
2024-12-09 23:07:57 +0100

    upload feature

--- src/components/profiles/list/ExportBtn.vue
+++ src/components/profiles/list/ExportBtn.vue
@@ -5,7 +5,7 @@
       icon="import_export"
       @click="dialog=true"
     >
-      manage
+      Manage export
     </q-btn>
     <q-dialog
       v-model="dialog"
@@ -24,14 +24,18 @@
                 Export
               </div>
               <p class="q-mt-md">
-                Export your profiles for another app.
+                Export your profiles for another app or device.
               </p>
+              <q-input
+                v-model="prefix"
+                label="Rifle name prefix"
+              />
             </q-card-section>
             <q-card-actions>
               <q-btn
                 icon="file_download"
                 color="primary"
-                @click="exportAsJson"
+                @click="exportProfiles"
               >
                 Export
               </q-btn>
@@ -48,16 +52,21 @@
                 Import
               </div>
               <p class="q-mt-md">
-                Insert within the app new profiles.
+                Insert new profiles within the app.
               </p>
             </q-card-section>
             <q-card-actions>
-              <q-btn
-                color="primary"
-                icon="file_upload"
+              <q-file
+                v-model="uploadedFile"
+                label="Upload file"
+                filled
+                accept=".json"
+                @update:model-value="fileUploaded"
               >
-                import
-              </q-btn>
+                <template #prepend>
+                  <q-icon name="attach_file" />
+                </template>
+              </q-file>
             </q-card-actions>
           </q-card>
         </div>
@@ -72,14 +81,32 @@ import { useProfilesStore } from 'stores/profiles'
 
 const dialog = ref(false)
 
+const date = new Date()
+const formattedDate = `${date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`
+
+const prefix = ref(`exported ${formattedDate}`)
+
 // list profiles
 const profilesStore = useProfilesStore()
 
-// remove id fields from profiles
-const sanitizedProfiles = profilesStore.profilesArray.map(({ id, ...rest }) => rest)
+// Add prefix to each rifle name
+function prefixRifleNames (profilesArray) {
+  return profilesArray.map((item) => ({
+    ...item,
+    weapon: {
+      ...item.weapon,
+      name: `${prefix.value} ${item.weapon.name}`
+    }
+  }))
+}
 
-function exportAsJson () {
-  const jsonString = JSON.stringify(sanitizedProfiles, null, 2) // Pretty-print with 2 spaces
+// remove id fields from profiles
+function sanitizedProfiles (profilesArray) {
+  return profilesArray.map(({ id, ...rest }) => rest)
+}
+
+function downloadJson (profilesArray) {
+  const jsonString = JSON.stringify(profilesArray, null, 2) // Pretty-print with 2 spaces
   const blob = new Blob([jsonString], { type: 'application/json' })
   const url = URL.createObjectURL(blob)
 
@@ -87,13 +114,42 @@ function exportAsJson () {
   a.href = url
 
   // format filename with year.month.day.profiles.json
-  const date = new Date()
-  const formattedDate = `${date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`
   const filename = `${formattedDate}.profiles.json`
 
   a.download = filename
   a.click()
   URL.revokeObjectURL(url)
+}
+
+function exportProfiles () {
+  const profilesArray = ref(profilesStore.profilesArray)
+  profilesArray.value = sanitizedProfiles(profilesArray.value)
+  profilesArray.value = prefixRifleNames(profilesArray.value)
+  downloadJson(profilesArray.value)
+}
+
+const uploadedFile = ref(null) // Holds the selected file
+const uploadedProfiles = ref('')
+function fileUploaded () {
+  if (uploadedFile.value) {
+    const fileReader = new FileReader()
+    fileReader.onload = function (event) {
+      uploadedProfiles.value = JSON.parse(fileReader.result)
+      insertProfilesIntoProfilesStore(uploadedProfiles)
+      // clear the upload field
+      uploadedFile.value = null
+      // close dialog
+      dialog.value = false
+    }
+    fileReader.readAsText(uploadedFile.value)
+  }
+}
+
+function insertProfilesIntoProfilesStore (uploadedProfiles) {
+  console.log(uploadedProfiles.value)
+  for (const profile of uploadedProfiles.value) {
+    profilesStore.addProfile(profile)
+  }
 }
 
 </script>

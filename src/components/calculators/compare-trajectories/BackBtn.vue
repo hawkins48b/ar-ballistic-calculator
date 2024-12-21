c8af2a4ecf7b43f0c5ab1121e5cf4867f4e697f3
quem <hawkins48b@gmail.com>
2024-12-21 20:53:13 +0100

    ui dev

---
+++ src/components/calculators/compare-trajectories/BackBtn.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-btn
+    flat
+    to="/calculators"
+    icon="arrow_back"
+  />
+</template>
+
+<script setup>
+</script>
--- src/pages/calculators/compare-trajectories/CompareTrajectoriesPage.vue
+++ src/pages/calculators/compare-trajectories/CompareTrajectoriesPage.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    hello compare trajectories
+    <div class="row">
+      <div class="col-auto">
+        <BackBtn />
+      </div>
+    </div>
+    <TrajectoriesList />
+    <TrajectoriesChart />
   </div>
 </template>
 
 <script setup>
+import BackBtn from 'components/calculators/compare-trajectories/BackBtn.vue'
+import TrajectoriesList from 'components/calculators/compare-trajectories/TrajectoriesList.vue'
+import TrajectoriesChart from 'components/calculators/compare-trajectories/TrajectoriesChart.vue'
+
 </script>

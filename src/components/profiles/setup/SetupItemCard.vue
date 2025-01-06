3e27235389476e69b49e37bfbaa913771420bbb4
Quentin Mathey <q.mathey@gmail.com>
2025-01-06 12:24:43 +0100

    rework profile item

--- src/components/profiles/setup/SetupItemCard.vue
+++ src/components/profiles/setup/SetupItemCard.vue
@@ -21,98 +21,111 @@
           <slot />
         </div>
       </div>
-      <div
+      <q-card
         v-if="setup.firearm"
-        class="row items-center q-gutter-md"
-        style="height:50px"
       >
-        <div class="col-auto">
-          <q-icon
-            name="svguse:icons.svg#sniper-rifle"
-            size="sm"
-          />
-        </div>
-        <div calss="col">
-          {{ setup.firearm.name }}
-        </div>
-      </div>
-      <div
+        <q-card-section>
+          <div
+            class="row items-center q-gutter-md"
+          >
+            <div class="col-auto">
+              <q-icon
+                name="svguse:icons.svg#sniper-rifle"
+                size="md"
+              />
+            </div>
+            <div calss="col">
+              {{ setup.firearm.name }}
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card
         v-if="setup.optic"
-        class="row items-center q-gutter-md"
-        style="height:50px"
+        class="q-mt-sm"
       >
-        <div class="col-auto">
-          <q-icon
-            :name="opticStore.opticIcon(setup.optic.type)"
-            size="sm"
-          />
-        </div>
-        <div calss="col">
-          {{ setup.optic.name }}
-        </div>
-      </div>
-      <div
-        class="row q-gutter-x-md"
-        style="height:40px"
-      >
-        <div
-          class="col"
-          style="padding-left:40px"
-        >
-          <div class="row  items-center q-gutter-x-md">
+        <q-card-section>
+          <div
+            class="row items-center q-gutter-md"
+          >
             <div class="col-auto">
               <q-icon
-                name="svguse:icons.svg#height"
+                :name="opticStore.opticIcon(setup.optic.type)"
                 size="md"
               />
             </div>
-            <div class="col">
-              {{ setup.measureOpticHeight }} {{ setup.measureOpticHeightUnit }}
+            <div calss="col">
+              {{ setup.optic.name }}
             </div>
           </div>
-        </div>
-        <div class="col">
-          <div class="row items-center q-gutter-x-md">
-            <div class="col-auto">
-              <q-icon
-                name="svguse:icons.svg#human-target"
-                size="md"
-              />
+          <div
+            class="row q-gutter-x-md q-mt-sm"
+          >
+            <div
+              class="col"
+            >
+              <div class="row  items-center q-gutter-x-md">
+                <div class="col-auto">
+                  <q-icon
+                    name="svguse:icons.svg#height"
+                    size="md"
+                  />
+                </div>
+                <div class="col">
+                  {{ setup.measureOpticHeight }} {{ setup.measureOpticHeightUnit }}
+                </div>
+              </div>
             </div>
             <div class="col">
-              {{ setup.measureZeroDistance }} {{ setup.measureZeroDistanceUnit }}
+              <div class="row items-center q-gutter-x-md">
+                <div class="col-auto">
+                  <q-icon
+                    name="svguse:icons.svg#human-target"
+                    size="md"
+                  />
+                </div>
+                <div class="col">
+                  {{ setup.measureZeroDistance }} {{ setup.measureZeroDistanceUnit }}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div
+        </q-card-section>
+      </q-card>
+
+      <q-card
         v-if="setup.ammunition"
-        class="row items-center q-gutter-md"
+        class="q-mt-sm"
       >
-        <div class="col-auto">
-          <q-icon
-            name="svguse:icons.svg#ammunition"
-            size="sm"
-          />
-        </div>
-        <div calss="col">
-          {{ setup.ammunition.name }}
-        </div>
-      </div>
-      <div class="row items-center q-gutter-x-md">
-        <div
-          class="col-auto"
-          style="padding-left:40px"
-        >
-          <q-icon
-            name="svguse:icons.svg#bullet-speed"
-            size="md"
-          />
-        </div>
-        <div class="col">
-          {{ setup.measureVelocity }} {{ setup.measureVelocityUnit }} at {{ setup.measureVelocityTemperature }} {{ setup.measureVelocityTemperatureUnit }}
-        </div>
-      </div>
+        <q-card-section>
+          <div
+            class="row items-center q-gutter-md"
+          >
+            <div class="col-auto">
+              <q-icon
+                name="svguse:icons.svg#ammunition"
+                size="sm"
+              />
+            </div>
+            <div calss="col">
+              {{ setup.ammunition.name }}
+            </div>
+          </div>
+          <div class="row items-center q-gutter-x-md q-mt-sm">
+            <div
+              class="col-auto"
+            >
+              <q-icon
+                name="svguse:icons.svg#bullet-speed"
+                size="md"
+              />
+            </div>
+            <div class="col">
+              {{ setup.measureVelocity }} {{ setup.measureVelocityUnit }} at {{ setup.measureVelocityTemperature }} {{ setup.measureVelocityTemperatureUnit }}
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
     </q-card-section>
   </q-card>
 </template>
